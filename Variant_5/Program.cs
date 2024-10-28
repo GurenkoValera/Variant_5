5151e2be83269a149bf4a3b317ddaa082d915a4d
valer <valer@DESKTOP-QD94NVR>
2024-10-28 19:45:35 +0300

    Добавьте файлы проекта.

---
+++ Variant_5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите день (число):");
+        int day = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите месяц (число):");
+        int month = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите год (число):");
+        int year = Convert.ToInt32(Console.ReadLine());
+
+        // Форматируем строку даты с добавлением нулей, если нужно
+        string formattedDate = $"{day:D2}/{month:D2}/{year}";
+
+        // Выводим результат
+        Console.WriteLine(formattedDate);
+    }
+}
